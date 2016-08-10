11a35b4daefc6ddaed87745c187e397e6a8e5332
ollie1400 <otlg123@gmail.com>
2016-08-10 14:07:23 +0100

    Added commands as QString objects without the trailing boundary character  (in future will remove the QByteArray versions)

--- def_commands.h
+++ def_commands.h
@@ -12,6 +12,13 @@ const QByteArray SETUP = "SETUP;";
 const QByteArray STREAM = "STREAM;";
 const QByteArray F_BOUNDARY = ";";
 
+const QString sGO = "GO";
+const QString sREADY = "RDY";
+const QString sDONE = "DONE";
+const QString sSETUP = "SETUP";
+const QString sSTREAM = "STREAM";
+const QString sF_BOUNDARY = ";";
+
 
 
 }
